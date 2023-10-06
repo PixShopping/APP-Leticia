9bc71158d054a8efa739a156ef3024c64d696791
Pixshopping021 <133106309+PixShopping@users.noreply.github.com>
2023-10-06 11:17:47 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,6 @@
-console.log("TESTANDO...")
+function logTestando() {
+  console.log("Testando");
+}
+
+// Configurar um loop que executa logTestando a cada 2 segundos
+setInterval(logTestando, 2000);
