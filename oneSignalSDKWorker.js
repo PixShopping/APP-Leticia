ee6acdc2d6941786fb4c161cfe0a136383f484e8
Pixshopping021 <133106309+PixShopping@users.noreply.github.com>
2023-10-05 23:54:41 -0300

    Add files via upload

---
+++ oneSignalSDKWorker.js
@@ -0,0 +1,7 @@
+importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js");
+window.OneSignalDeferred = window.OneSignalDeferred || [];
+        OneSignalDeferred.push(function(OneSignal) {
+            OneSignal.init({
+            appId: "7f7ebe08-4826-4b89-a30e-2232f5fd59b5",
+            });
+        });
